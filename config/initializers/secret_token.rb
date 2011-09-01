8159c1ede45c911c612990e4bee3b27b8c70a483
boyu2011 <boyu2011@gmail.com>
2011-09-01 17:44:50 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostNow::Application.config.secret_token = 'fbbc610c304ea7d5b00c223f61bff795935ed3ceb4873da7a90b02249f9d887a8e937da406a510851ef08e2d8d5b1827f5a97075a7901329c19fb0dbf650d5f6'
